65750cc9db29ff8952f8eaf9bdc20e491315c33c
Akuna Joshua <akunajoshua@gmail.com>
2023-02-17 07:03:57 -0800

    first correction for task: 0-positive_or_negative commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main - The program's entry point.
  *
  * Description: 'The program generates a random number n and prints
- * 	whether n is greater than, less than or equal to 0'
+ * whether n is greater than, less than or equal to 0'
  *
  * Return: Always 0 (if successful)
  */
@@ -19,12 +19,15 @@ int main(void)
 	/*Initialize the random number generator */
 	srand(time(0));
 	/*Initialize n to a randomly generated number*/
-	n = rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 
 	/*check if n is greater than, less than or equals to 0*/
-	if (n > 0) printf("%d is positive\n", n);
-	else if (n < 0) printf("%d is negative\n", n);
-	else printf("%d is zero\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 
 	/*returns 0 if no Exception occured*/
 	return (0);
