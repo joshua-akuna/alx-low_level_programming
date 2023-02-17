c7e2996018de346b171c6d936bc2c30d3a6302e4
Akuna Joshua <akunajoshua@gmail.com>
2023-02-16 17:11:45 -0800

    Initial answers to the advanced tasks commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The program's entry point.
+ *
+ * Description: 'prints a string to the standard error'
+ *
+ * Return: Returns 0 (if successful)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a mutilingual puzzle");
+	puts("\"Programming is like building a mutilingual puzzle");
 	return (0);
 }
