507c7cd0c0a4acf16abcc7ca004e09dcc1b59307
Akuna Joshua <akunajoshua@gmail.com>
2023-02-16 18:10:11 -0800

    fourth correction for 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\"";
-	char str2[] = "- Dora Korapar, 2015-10-19\n";
+	char str2[] = "- Dora Korpar, 2015-10-19\n";
 
 	fprintf(stderr, "%s %s", str1, str2);
 	return (1);
