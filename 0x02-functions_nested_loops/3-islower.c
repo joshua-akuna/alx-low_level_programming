69a8fb18830ab1c494880015f766e7c286ec6689
Akuna Joshua <akunajoshua@gmail.com>
2023-02-21 09:12:56 -0800

    task: 3-islower initial files commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - the functions determines if c parameter is a
+ * lowercase alphabet or not.
+ * @c: an int argument to be checked.
+ * Return: returns (0) if its a lowercase alphabet, else return (1)
+ */
+int _islower(int c)
+{
+	int a, z;
+
+	a = 'a';
+	z = 'z';
+
+	if (c < a || c > z)
+		return (1);
+	return (0);
+}
