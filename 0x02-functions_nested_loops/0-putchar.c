44e0ea791b2daece365f482da8f10c4cb0f02d02
Akuna Joshua <akunajoshua@gmail.com>
2023-02-21 06:48:31 -0800

    task: 0-putchar initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+/**
+ * main - the entry point of the program.
+ * Description: 'calls the custom function _putchar to print the string
+ * "_putchar"'
+ * Return: return (0) always if success.
+ */
+
+int main(void)
+{
+	char *ptr = "_putchar";
+
+	while (*ptr)
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
+	_putchar('\n');
+	return (0);
+}
