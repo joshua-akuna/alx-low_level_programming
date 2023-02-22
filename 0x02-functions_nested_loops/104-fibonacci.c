8b71e335d11a194be1da97c0c41c2c52aa10025e
Akuna Joshua <akunajoshua@gmail.com>
2023-02-21 19:38:26 -0800

    task 104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,39 +8,43 @@
 
 int main(void)
 {
-	long int fib, cur, prev, count;
-	long int fib1, fib2, div;
+	unsigned long fib = 0, cur = 1, prev = 0, count;
+	unsigned long fib1_div1, fib1_div2, fib2_div1, fib2_div2;
+	unsigned long div1, div2, divisor = 10000000000;
 
-	fib = 0;
-	cur = 1;
-	prev = 0;
-	div = 10000000000;
-
-	for (count = 0; count < 98; count++)
+	for (count = 0; count < 92; count++)
 	{
 		fib = cur + prev;
-
-		if (fib < 0)
-		{
-			fib1 = (fib / div) * ((fib > 1) - (fib < 1));
-			fib2 = (fib % div) * ((fib > 1) - (fib < 1));
-			printf("%ld%ld", fib1, fib2);
-		}
-		else
-		{
-			printf("%ld", fib);
-		}
-
-		if (count == 97)
-			break;
-
-		printf(", ");
-
+		printf("%lu, ", fib);
+		
 		prev = cur;
 		cur = fib;
 	}
+	fib1_div1 = cur / divisor;
+	fib2_div1 = prev / divisor;
+	fib1_div2 = cur % divisor;
+	fib2_div2 = prev % divisor;
 
+	for (count = 93; count < 99; count++)
+	{
+		div1 = fib1_div1 + fib2_div1;
+		div2 = fib1_div2 + fib2_div2;
+
+		if (div2 > 9999999999)
+		{
+			div1 += 1;
+			div2 %= 10000000000;
+		}
+		printf("%lu%lu", div1, div2);
+
+		if (count != 97)
+			printf(", ");
+
+		fib1_div1 = fib2_div1;
+		fib1_div2 = fib2_div2;
+		fib2_div1 = div1;
+		fib2_div2 = div2;
+	}
 	putchar('\n');
-
 	return (0);
 }
