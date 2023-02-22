5eb11f718b144d6910a6069e6d3479f9ed7f503e
Akuna Joshua <akunajoshua@gmail.com>
2023-02-21 16:08:58 -0800

    task 10-add.c files commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @num1: first int parameter
+ * @num2: second int parameter
+ * Return: the sum of the integers
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
