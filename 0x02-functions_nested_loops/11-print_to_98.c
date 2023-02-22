1a2669b432616e37b54674bbdf32e3e7e3719720
Akuna Joshua <akunajoshua@gmail.com>
2023-02-21 16:25:16 -0800

    task 11-print_to_98 file commite

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from input 'n'to 98.
+ *
+ * @n: the integer from which counting begins.
+ *
+ * Return: has no return statement.
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+	}
+	else
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+	}
+	print("%d\n", n);
+}
