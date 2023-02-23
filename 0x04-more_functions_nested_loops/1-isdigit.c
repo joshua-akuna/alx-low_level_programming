601d0af557f6ed7b0924a2343929791d981cefdb
Akuna Joshua <akunajoshua@gmail.com>
2023-02-23 05:03:19 -0800

    task 1-isdigit.c commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+/**
+ * _isdigit - checks if the parameter is a digit.
+ * @c: a character argument.
+ * Return: 1 if c is a digit else 0.
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
