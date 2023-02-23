aaa3802a9abe4e9bf1c92a3fc9b22fd234773572
Akuna Joshua <akunajoshua@gmail.com>
2023-02-23 05:15:24 -0800

    task 6-print_line.c file commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line: prints '_', 'n' times.
+ * @n: an integer parameter.
+ */
+
+void print_line(int n)
+{
+	int count;
+
+	if (n > 0)
+	{
+		for (count = 0; count < n; count++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
