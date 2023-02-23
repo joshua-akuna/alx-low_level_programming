74f2599e538683550253d0d430bcd36fa4ceec6a
Akuna Joshua <akunajoshua@gmail.com>
2023-02-23 05:09:20 -0800

    task 2-mul.c file commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - returns the product of 2 integers.
+ * @a: first integer argument.
+ * @b: second integer argument.
+ * Return: product of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
