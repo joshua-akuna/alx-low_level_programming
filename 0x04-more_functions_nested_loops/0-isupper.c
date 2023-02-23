343c5cc4755a0aacdf52891ae8382577e9fbe8f2
Akuna Joshua <akunajoshua@gmail.com>
2023-02-23 00:45:21 -0800

    task 0-isupper.c file commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - checks if a character is uppercase.
+ * @c: an int argument.
+ * Return: 1 if uppercase alphabet else 0.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a description for the tasks done for ALXSE project 0x04-more_functions_nested_loops
