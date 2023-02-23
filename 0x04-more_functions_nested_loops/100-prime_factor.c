d8b783fe2bca7b4dbeecd8cc7ead50589f4f5399
Akuna Joshua <akunajoshua@gmail.com>
2023-02-23 05:51:55 -0800

    task 100-prime_factor.c commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - determine the largest prime factor of a specified number.
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int num = 612852475143;
+	int pf = 2;
+	int largest = 0;
+
+	while (num > 1)
+	{
+		if (num % pf == 0)
+		{
+			largest = pf > largest ? pf : largest;
+			num /= pf;
+			continue;
+		}
+		pf++;
+	}
+	printf("%d\n", largest);
+
+	return (0);
+}
