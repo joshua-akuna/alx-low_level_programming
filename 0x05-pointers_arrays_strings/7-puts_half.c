0c3f473572d57fd6ff5de70650a8716db3cac34b
Akuna Joshua <akunajoshua@gmail.com>
2023-02-27 14:34:52 -0800

    7-puts_half.c file correction_1 commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,14 @@
 
 void puts_half(char *str)
 {
-	unsigned int i, len, temp;
+	unsigned int i, len, half_len, temp;
 
 	len = strlen(str);
+	half_len = len / 2;
+	if (len % 2 == 1)
+		half_len++;
 
-	for (i = len/2; i < len; i++)
+	for (i = half_len; i < len; i++)
 	{
 		temp = str[i] - '0';
 		_putchar(temp + '0');
