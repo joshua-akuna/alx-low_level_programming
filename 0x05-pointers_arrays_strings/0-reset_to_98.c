f38f46d7dfc8b36ddb3eba1aeb71152e1be6befb
Akuna Joshua <akunajoshua@gmail.com>
2023-02-27 11:18:41 -0800

    0-reset_to_98.c file initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ * reset_to_98 - takes a pointer to an int and update its value.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a description of the tasks for ALXSE 0x05-pointers_arrays_strings project
