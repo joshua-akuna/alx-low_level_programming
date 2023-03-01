a2877f9753c3a3c0c98f274f679780dfd9114715
Akuna Joshua <akunajoshua@gmail.com>
2023-03-01 12:47:41 -0800

    2-strncpy.c file correction_1 commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,6 +2,7 @@
  * _strncpy - copies a string
  * @dest: char pointer to a string to copy to.
  * @src: char pointer to a string to copy from.
+ * @n: int parameter for number of bytes to copy.
  * Return: always dest.
  */
 
@@ -9,10 +10,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < (unsigned)n && src[i] != '\0'; i++)
+	for (i = 0; i < (unsigned int)n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	for (; i < (unsigned)n; i++)
+	for (; i < (unsigned int)n; i++)
 		dest[i] = '\0';
 
 	return (dest);
