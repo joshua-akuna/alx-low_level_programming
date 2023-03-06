455027285cf8b661e866699d4b4daf0af1bae972
Akuna Joshua <akunajoshua@gmail.com>
2023-03-06 12:35:26 -0800

    2-strchr.c file initial commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+/**
+ * _strchr - returns a pointer to the first occurence of the character 'c'
+ * in the string 's', or NULL if the character is not foound.
+ * @s: pointer to a string to be scanned.
+ * @c: character to scan for
+ * Return: a pointer to the first occurrence of the character 'c'
+ * or "NULL" if 'c' is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
