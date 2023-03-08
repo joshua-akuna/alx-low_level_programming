23296ff5abe64f4607e5f313b1371d8bc44ec012
Akuna Joshua <akunajoshua@gmail.com>
2023-03-08 09:30:33 -0800

    5-sqrt_recursion.c file initial recommit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,10 @@
 int sqrt_recursion(int n, int guess);
 
 /**
- * _sqrt_recursion - calculates the square root of a number.
+ * _sqrt_recursion- calculates the square root of a number.
  * @n: an int argument whose square root to determine.
  * Return: -1 if 'n' has no natural square root else the calculated square root.
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -21,12 +20,11 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * sqrt_recursion - determines the square root of a number.
+ * sqrt_recursion- determines the square root of a number.
  * @n: an int argument whose square root to determine.
  * @guess: a random int argument which could be the natural square root of 'n'.
  * Return: -1 if 'n' has no natural square root else the calculated square root.
  */
-
 int sqrt_recursion(int n, int guess)
 {
 	if (guess * guess == n)
