a4f44a21b709f13007060892fca4df837d6c07aa
Akuna Joshua <akunajoshua@gmail.com>
2023-03-10 11:15:38 -0800

    1-args.c file initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prkints the number of arguments passed to this program.
+ * @argc: int argument for the size of the array "argv".
+ * @argv: a string array.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
