41e9b4be1e5effb02f1ddec36fd362382b9aa544
Akuna Joshua <akunajoshua@gmail.com>
2023-03-14 13:01:56 -0700

    100-argstostr.c file correction_2: wrong allocated mem

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * word_len - returns the length of a string.
+ * @str: a char pointer to a string.
+ * Return: an integer.
+ */
+int word_len(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	return (1 + word_len(str + 1));
+}
+
 /**
  * argstostr - contatenates all the strings in an input array argument.
  * @ac: count of strings in "av".
@@ -9,14 +22,15 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, len = 0;
+	int i, j, len = 0, sub_len = 0;
 	char *str, *sub_str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-		len += sizeof(*(av + i)) + 1;
+		len += word_len(*(av + i)) + 1;
+	len++;
 
 	str = malloc(sizeof(char) * len);
 
@@ -27,19 +41,16 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		sub_str = *(av + i);
+		sub_len = word_len(sub_str);
 
-		while (*sub_str != '\0')
+		for (j = 0; j < sub_len; j++)
 		{
-			*(str + len) = *sub_str;
-			sub_str++;
-			len++;
+			str[len++] = sub_str[j];
 		}
 
-		if (i != ac - 1)
-			*(str + len++) = '\n';
+		str[len++] = '\n';
 	}
+	str[len++] = '\0';
 
-	printf("%s\n", str);
-
-	return (NULL);
+	return (str);
 }
