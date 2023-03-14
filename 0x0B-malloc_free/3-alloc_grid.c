44c3230606f80d4db618026bc4957e583ed97a9c
Akuna Joshua <akunajoshua@gmail.com>
2023-03-14 08:10:52 -0700

    3-alloc_grid.c file corection_5:betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -38,7 +38,7 @@ int **alloc_grid(int width, int height)
 		*(grid + i) = malloc(sizeof(int) * width);
 		if (*(grid + i) == NULL)
 		{
-			for (;i >= 0; i--)
+			for ( ; i >= 0; i--)
 				free(*(grid + i));
 			free(grid);
 			return (NULL);
