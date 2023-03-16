5746f7049557f72069ca53c66d33e35c69173bd5
Akuna Joshua <akunajoshua@gmail.com>
2023-03-16 13:56:53 -0700

    3-array_range.c file correction_1:betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * array_range - returns an array contining the values of the range from "min" to "max", both inclusive.
+ * array_range - returns an array contining the values
+ * of the range from "min" to "max", both inclusive.
  * @min: an integer argument.
  * @max: an integer argument.
- * Return: "NULL" if malloc fails or min is greater than max else return a pointer to the dynamically created array of integers.
+ * Return: "NULL" if malloc fails or min is greater than
+ * max else return a pointer to the dynamically created array of integers.
  */
 
 int *array_range(int min, int max)
