0a5a36a31f90c6cc414134a8a466d962c4a2bfc0
joshua-akuna <akunajoshua.com>
2023-03-20 15:03:39 +0100

    dog.h file: correction_1: added description for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog {
+/**
+ * struct dog - a struct defining a dog.
+ * @name: a string attribute.
+ * @age: a float attributre.
+ * @owner: a string attribute.
+ *
+ * Description: This is a struct that defines a dog
+ * with three attributes name, age and owner.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
