2347bdf4d885437585e8ca2b7b28e2f77cdb24d1
joshua-akuna <akunajoshua.com>
2023-03-21 02:24:57 +0100

    4-new_dog.c file: correction_1: output failure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,71 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_copy - dynamically allocate a space for the string argument.
+ * @str: a parameter of type string.
+ * Return: pointer to the dynamically allocated space.
+ */
+char *malloc_copy(char *str)
+{
+	int i = 0, len = 0;
+	char *ptr;
+
+	while (*(str + i++))
+		len++;
+	ptr = malloc(len + 1);
+
+	if (!ptr)
+		return (0);
+
+	for (i = 0; i < len; i++)
+		ptr[i] = str[i];
+	ptr[i] = 0;
+
+	return (ptr);
+}
+
 /**
  * new_dog - creates a variable of type dog_t.
  * @name: a variable of type string.
  * @age: a variable of type float;
  * @owner: a variable of type string.
+ * Return: a ptr to a dynamically allocated memory block for a
+ * new instance of a type dog_t variable else false if function fails.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog_ptr;
 	char *_name;
 	char *_owner;
-	dog_t my_dog;
-	dog_t *dog_ptr;
 
-	_name = name? name : "";
-	_owner = owner? owner : "";
+	if (!name || !owner)
+		return (0);
 
-	my_dog.name = _name;
-	my_dog.age = age;
-	my_dog.owner = _owner;
-
-	dog_ptr = &my_dog;
-
+	dog_ptr = malloc(sizeof(dog_t));
 	if (!dog_ptr)
-		return 0;
+		return (0);
+
+	_name = malloc_copy(name);
+	if (!_name)
+	{
+		free(dog_ptr);
+		return (0);
+	}
+
+	_owner = malloc_copy(owner);
+	if (!_owner)
+	{
+		free(_name);
+		free(dog_ptr);
+		return (0);
+	}
+
+	dog_ptr->name = _name;
+	dog_ptr->age = age;
+	dog_ptr->owner = owner;
 
 	return (dog_ptr);
 }
