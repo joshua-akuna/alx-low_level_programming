d87477e62e4a1c58f1cee0e339f90c065ece8918
joshua-akuna <akunajoshua.com>
2023-03-23 09:16:37 +0100

    2-print_strings.c file : first commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints a list of strings seperated
+ * by a seperator.
+ * @separator: a string printed between the string arguments..
+ * @n: number of variable int arguments.
+ * @...: list of variable int arguments.
+ * Return: nothing.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator && i && i < n)
+			printf("%s", separator);
+
+		printf("%s", va_arg(args, char *));
+
+		if (i == n - 1)
+			printf("\n");
+	}
+
+	va_end(args);
+}
