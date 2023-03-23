1575b52455c98928a2215a3710b2f0205f29df6d
joshua-akuna <akunajoshua.com>
2023-03-23 17:17:50 +0100

    2-print_strings.c file : correction 1 : output failure

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,10 +21,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 
 		printf("%s", va_arg(args, char *));
-
-		if (i == n - 1)
-			printf("\n");
 	}
 
 	va_end(args);
+	putchar('\n');
 }
