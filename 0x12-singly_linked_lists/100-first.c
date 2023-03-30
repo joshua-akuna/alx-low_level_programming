9e06fb0bfe649e8ef71b09e16392b2c33769c096
Joshua Akuna <akunajoshua@gmail.com>
2023-03-30 21:24:08 +0100

    100-first.c task : correction 3 : betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
  */
 void before_main(void)
 {
-	char *str = "You're beat! and yet, you must allow\n";
+	char *str = "You're beat! and yet, you must allow,\n";
 
 	printf("%sI bore my house upon my back!\n", str);
 }
