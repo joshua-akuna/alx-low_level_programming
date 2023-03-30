fe0fadc28a448891b0eb380cb20a4b500f99037e
Joshua Akuna <akunajoshua@gmail.com>
2023-03-30 21:18:29 +0100

    100-first.c task : correction 1 : betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 /**
  * before_main - prints "You're beat! and yet, you must allow,
- * \nI bore my house upon my back!\n"
- * Return: nothin,
+ * \nI bore my house upon my back!\n" before the main function
+ * executes.
+ * Return: nothing.
  */
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n\
-I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI \
+bore my house upon my back!\n");
 }
