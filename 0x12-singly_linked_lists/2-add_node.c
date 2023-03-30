6c44799ec9e119e6a27415b8e10ddead80e44222
Joshua Akuna <akunajoshua@gmail.com>
2023-03-30 15:43:15 +0100

    2-add_node.c task : code refactored

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,28 +9,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node = NULL;
 	size_t len = 0;
 
-	if (!str || !head)
-		return (NULL);
+	if (str && head)
+	{
+		new_node = malloc(sizeof(list_t));
+		
+		if (!new_node)
+			return (NULL);
 
-	new_node = malloc(sizeof(list_t));
-
-	if (!new_node)
-		return (NULL);
-
-	new_node->str = strdup((char *)str);
-
-	while (str[len])
-		len++;
-
-	if (len <= 0)
-		return (NULL);
-
-	new_node->len = len;
-	new_node->next = *head;
-	*head = new_node;
+		while (str[len])
+			len++;
+		
+		new_node->str = strdup((char *)str);
+		new_node->len = len;
+		new_node->next = *head;
+		*head = new_node;
+	}
 
 	return (new_node);
 }
