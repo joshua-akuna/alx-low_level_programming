f1a4affd0fadbf551498819c3b363c23caa0abb0
Joshua Akuna <akunajoshua@gmail.com>
2023-03-30 21:09:37 +0100

    100-first.c task : first commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before_main - prints "You're beat! and yet, you must allow,
+ * \nI bore my house upon my back!\n"
+ * Return: nothin,
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n\
+I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/tests/100-main.c
@@ -0,0 +1,10 @@
+#include "../lists.h"
+/**
+ * main - test code for the 100-first.c program.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
