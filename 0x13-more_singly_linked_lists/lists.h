018e2a5639fa1842fa1e944fd3c054b9170ad997
Joshua Akuna <akunajoshua@gmail.com>
2023-04-04 16:43:32 +0100

    lists.h : update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LOOP_END 2
+
 /**
  * struct listint_s - singly linked list.
  * @n: an integer.
@@ -32,5 +34,6 @@ listint_t *insert_nodeint_at_index(
 		listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
 
 #endif
