5f411e89995983519ef8e19f93588b890c2d81de
Joshua Akuna <akunajoshua@gmail.com>
2023-04-04 15:15:25 +0100

    9-insert_nodeint.c : correction 2 : check 8, 10, 11 failed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = current_node;
-		return (current_node);
+		return (new_node);
 	}
 
 	/* finds the node at idx - 1 */
