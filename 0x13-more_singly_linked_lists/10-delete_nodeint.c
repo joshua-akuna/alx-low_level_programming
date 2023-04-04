e49dc140a24a3aac6a0b17067d09a1cac2e50eef
Joshua Akuna <akunajoshua@gmail.com>
2023-04-04 14:34:34 +0100

    10-delete_nodeint.c : correction 1 : betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_nodeint - deletes the node at index index
+ * delete_nodeint_at_index - deletes the node at index index
  * of a listint_t linked list.
  * @head: a pointers to the singly linked list.
  * @index: index whose node is to be deleted.
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node_before_index;
 	unsigned int i;
 
-	if(!head || !(*head))
+	if (!head || !(*head))
 		return (-1);
 
 	if (index == 0)
@@ -34,12 +34,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (node_before_index == NULL || node_at_index == NULL)
 			return (-1);
 
-		/* assign the node next to the one to be deleted as
+		/*
+		 * assign the node next to the one to be deleted as
 		 * the next to the node before the one to be deleted.
 		 */
 		node_before_index->next = node_at_index->next;
 	}
-	
 	/* delete the node at index index */
 	free(node_at_index);
 	node_at_index = NULL;
