4f00e0eaf271fc7803dc222f4ec71b1230387223
Joshua Akuna <akunajoshua@gmail.com>
2023-04-10 05:50:24 +0100

    1-create_file.c : correction 1 : checke 8, 9 output failed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,27 +3,31 @@
  * create_file - creates a file.
  * @filename: name of the file to create.
  * @text_content: content to be written to the newly created file.
- * Return: 1 if operation was successful else -1.
+ * Return: 1 if operation was successful.
+ * -1 if filename is NULL or on operation failure.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file_desc;
-	int len = 0;
+	int len = 0, bytes_out = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[len])
-		len++;
 
-	file_desc = open(filename, O_WRONLY | O_TRUNC | O_CREAT,
-			0640);
+	file_desc = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0640);
 	if (file_desc == -1)
 		return (-1);
 
 	if (text_content != NULL)
-		write(file_desc, text_content, len);
+	{
+		while (text_content[len])
+			len++;
+		bytes_out = write(file_desc, text_content, len);
+	}
 
+	if (bytes_out != len)
+		return (-1);
 	close(file_desc);
 
 	return (1);
