00b5ae924799b9c4d267ec12b035c0aa27a1dfb2
Joshua Akuna <akunajoshua@gmail.com>
2023-04-10 06:00:13 +0100

    2-append_text_to_file.c : correction 1 : checker 3, 8, 9, 10 output faiilure

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,17 +14,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[len])
-		len++;
-
-	printf("len : %i\n", len);
-
 	file_desc = open(filename, O_WRONLY | O_APPEND);
 	if (file_desc == -1)
 		return (-1);
 
 	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
 		write(file_desc, text_content, len);
+	}
 
 	close(file_desc);
 
