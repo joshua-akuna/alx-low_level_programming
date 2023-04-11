b051a890826ff1cb57c4f668ff3685650809e07d
Joshua Akuna <akunajoshua@gmail.com>
2023-04-11 01:42:24 +0100

    3-cp.c : refactored : checker 20 failed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,76 +1,69 @@
 #include "main.h"
 
-void check_file_desc(int file_from_desc, int file_to_desc, char **arg);
 /**
- * main - copies the content of a file to another.
- * @argc: number of arguments passed to main.
- * @argv: array of string of arguments passed to main.
- * Return: Always 0.
- * exit with code 98 if file_from does not exist or can't be read.
- * exit with code 99 if file_to cannot be created or if write
- * to file fails.
- * exit with code 100 if a file descriptor cannot close.
+ * checks_file_desc - checks if files can be opened.
+ * @file_from_desc: the file descriptor to copy from.
+ * @file_to_desc: file descriptor to copy to
+ * @argv: array of string arguments
  */
-int main(int argc, char **argv)
-{
-	int file_from_desc, file_to_desc, chk_close;
-	char buffer[1024];
-	ssize_t bytes_read, bytes_out;
-
-	if (argc != 3)
-	{
-		dprintf(2, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	file_from_desc = open(argv[1], O_RDONLY);
-	file_to_desc = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	check_file_desc(file_from_desc, file_to_desc, argv);
-
-	while (bytes_read == 1024)
-	{
-		bytes_read = read(file_from_desc, buffer, 1024);
-		if (bytes_read == -1)
-			check_file_desc(-1, 1, argv);
-		bytes_out = write(file_to_desc, buffer, bytes_read);
-		if (bytes_out == -1)
-			check_file_desc(1, -1, argv);
-	}
-
-	chk_close = close(file_from_desc);
-	if (chk_close == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_from_desc);
-		exit(100);
-	}
-
-	chk_close = close(file_to_desc);
-	if (chk_close == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to_desc);
-		exit(100);
-	}
-
-	return (0);
-}
-
-/**
- * check_file_desc - checks if the file descriptors are valid.
- * @arg: an array of strings
- * @file_from_desc: file descriptor of the file to read from.
- * @file_to_desc: file descriptor of the file to write to.
- * Return: nothing.
- */
-void check_file_desc(int file_from_desc, int file_to_desc, char **arg)
+void check_file_desc(int file_from_desc, int file_to_desc, char *argv[])
 {
 	if (file_from_desc == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", arg[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to_desc == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", arg[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
+}
+
+/**
+ * main - copies the content of one file to another.
+ * @argc: number of arguments passed to the main function.
+ * @argv: an array of strings passed to the main function.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from_desc, file_to_desc, chk_close;
+	ssize_t bytes_read = 1024, bytes_out;
+	char buffer[1024];
+	
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	
+	file_from_desc = open(argv[1], O_RDONLY);
+	file_to_desc = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	check_file_desc(file_from_desc, file_to_desc, argv);
+	
+	while (bytes_read == 1024)
+	{
+		bytes_read = read(file_from_desc, buffer, 1024);
+		if (bytes_read == -1)
+			check_file_desc(-1, 0, argv);
+		bytes_out = write(file_to_desc, buffer, bytes_read);
+		
+		if (bytes_out == -1)
+			check_file_desc(0, -1, argv);
+	}
+	chk_close = close(file_from_desc);
+	if (chk_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_desc);
+		exit(100);
+	}
+	
+	chk_close = close(file_to_desc);
+	if (chk_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_desc);
+		exit(100);
+	}
+return (0);
 }
