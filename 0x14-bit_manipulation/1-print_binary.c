8f9e2410fd2f15e2b876784df3b807f26aebcda3
Joshua Akuna <akunajoshua@gmail.com>
2023-05-04 10:57:09 +0100

    bit manipulation : 1-print_binary.c file commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: an unsigned int to convert to binary.
+ * Return: nothing.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int num = n;
+
+	if (num >= 2)
+		print_binary(num >> 1);
+	putchar((num & 1) + '0');
+}
