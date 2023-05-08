8b66c604f22b489c3f070dc8f5d97496fb2c0a3a
Joshua Akuna <akunajoshua@gmail.com>
2023-05-08 14:00:13 +0100

    fileIO : 3-cp.c file : refactored

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 void validate_fd(int fd_src, int fd_dest, char *file_names[]);
-
 /**
  * main - copies the content of one file to another.
  * @argc: number of arguments passed to the main function.
@@ -25,7 +24,7 @@ int main(int argc, char *argv[])
 	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	/* validates the file descriptors */
 	validate_fd(src, dest, argv);
-
+	/* transfer bytes from the src to the destination files */
 	while (nread == 1024)
 	{
 		/* read and validate bytes from the src file into the buffer */
@@ -50,6 +49,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
 		exit(100);
 	}
+	/* returns 0 if successful */
 	return (0);
 }
 
