b2a31956eeead38127810fb554dcd4a96a4045cc
Joshua Akuna <akunajoshua@gmail.com>
2023-06-15 07:42:34 +0100

    0x17-doubly_linked_list project initial setup

---
+++ 0x17-doubly_linked_list/README.md
@@ -0,0 +1,1 @@
+This is a description for the tasks defined in ALXSE 0x17-doubly_linked_list project
---
+++ 0x17-doubly_linked_list/lists.h
@@ -0,0 +1,6 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+
+#endif /* _LISTS_H_ */
