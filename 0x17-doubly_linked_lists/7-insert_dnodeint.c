8ccf2430e7005c09d4df96790b86bcf00ce8e69b
Joshua Akuna <akunajoshua@gmail.com>
2023-06-16 03:52:58 +0100

    7-insert_dnodeint.c : correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,33 +14,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int index = 0;
 
 	if (idx == 0)
-		new_node = add_dnodeint(h, n);
-	else
+		return add_dnodeint(h, n);
+	
+	while (node != NULL)
 	{
-		while (node != NULL)
+		if (idx - 1 == index)
 		{
-			if (idx - 1 == index)
-			{
-				/* adds the new node to the end of the linked list */
-				if (node->next == NULL)
-					return (add_dnodeint_end(h, n));
-				/* creates a new node */
-				new_node = malloc(sizeof(dlistint_t));
-				if (new_node == NULL)
-					return (NULL);
-				new_node->n = n;
-				/* attaches the new node to the linked list */
-				new_node->prev = node;
-				new_node->next = node->next;
-				node->next->prev = new_node;
-				node->next = new_node;
-				/* returns the newly created node */
-				return (new_node);
-			}
-			/* move to the next node */
-			index++;
-			node = node->next;
+			/* adds the new node to the end of the linked list */
+			if (node->next == NULL)
+				return (add_dnodeint_end(h, n));
+			/* creates a new node */
+			new_node = malloc(sizeof(dlistint_t));
+			if (new_node == NULL)
+				return (NULL);
+			new_node->n = n;
+			/* attaches the new node to the linked list */
+			new_node->prev = node;
+			new_node->next = node->next;
+			node->next->prev = new_node;
+			node->next = new_node;
+			/* returns the newly created node */
+			return (new_node);
 		}
+		/* move to the next node */
+		index++;
+		node = node->next;
 	}
-	return (new_node);
+	return (NULL);
 }
