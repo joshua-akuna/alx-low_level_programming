4b20a35a02419d21f6bbf2dcb898c89f31ac4877
Joshua Akuna <akunajoshua@gmail.com>
2023-06-16 04:29:35 +0100

    lists.h updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,24 +21,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (node != NULL)
 	{
-		if (idx - 1 == index)
+		if (idx == index)
 		{
-			/* adds the new node to the end of the linked list */
-			if (node->next == NULL)
-				return (add_dnodeint_end(h, n));
 			/* creates a new node */
 			new_node = malloc(sizeof(dlistint_t));
 			if (new_node == NULL)
 				return (NULL);
 			new_node->n = n;
 			/* attaches the new node to the linked list */
-			new_node->prev = node;
-			new_node->next = node->next;
-			node->next->prev = new_node;
-			node->next = new_node;
+			new_node->prev = node->prev;
+			node->prev->next = new_node;
+			node->prev = new_node;
+			new_node->next = node;
 			/* returns the newly created node */
 			return (new_node);
 		}
+		else if (idx == index + 1 && node->next == NULL)
+			return (add_dnodeint_end(h, n));
 		/* move to the next node */
 		index++;
 		node = node->next;
