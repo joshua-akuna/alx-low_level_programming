145fb195bc6bfd52f984383a435e940c22fe9969
Joshua Akuna <akunajoshua@gmail.com>
2023-06-16 03:59:56 +0100

    7-insert_dnodeint.c : correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int index = 0;
 
 	if (idx == 0)
-		return add_dnodeint(h, n);
-	
+		return (add_dnodeint(h, n));
+
 	while (node != NULL)
 	{
 		if (idx - 1 == index)
