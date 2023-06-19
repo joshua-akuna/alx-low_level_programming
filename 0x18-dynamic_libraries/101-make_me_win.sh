e9f8f9c65a2c4468c1d5190315adb006dad2e8e3
Joshua Akuna <akunajoshua@gmail.com>
2023-06-19 20:36:33 +0100

    putshack.so file updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/joshua-akuna/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshake.so
+wget -P /tmp https://github.com/joshua-akuna/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
