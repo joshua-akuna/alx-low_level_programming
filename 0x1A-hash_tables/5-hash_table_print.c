b0b41fcc3b3ba9de326eff885d448a6efd02f6d9
Joshua Akuna <akunajoshua@gmail.com>
2023-07-07 00:37:52 +0100

    5-hash_table_print.c files commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the contents of the hash table.
+ * @ht: the hash table.
+ * Return: Nothing.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i, flag = 0;
+	hash_node_t *node = NULL;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		for (node = ht->array[i]; node; node = node->next)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s' : '%s'", node->key, node->value);
+			flag = 1;
+		}
+	}
+	printf("}");
+	printf("\n");
+}
